dc6280fb3791fc3a10007f1ce7a78a82ee84af56
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DetailViewButton.Module.Web/ButtonClickHandlingWebController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Web.ASPxEditors;
+
+namespace DetailViewButton.Module.Web {
+    public partial class ButtonClickHandlingWebController : ViewController {
+        public ButtonClickHandlingWebController() {
+            TargetViewId = "Person_DetailView";
+            ViewControlsCreated += new EventHandler(ButtonClickHandlingWebController_ViewControlsCreated);
+        }
+        void ButtonClickHandlingWebController_ViewControlsCreated(object sender, EventArgs e) {
+            DetailView view = (DetailView)View;
+            foreach(ControlDetailItem item in view.GetItems<ControlDetailItem>()) {
+                if(item.Id == "MyButton One") {
+                    (item.Control as ASPxButton).Text = "MyButton One";
+                    (item.Control as ASPxButton).Click += new EventHandler(ButtonClickHandlingWebController_Click);
+                    break;
+                }
+            }
+        }
+        void ButtonClickHandlingWebController_Click(object sender, EventArgs e) {
+            // Perform the required actions on the server side.
+        }
+    }
+}
---
+++ CS/DetailViewButton.Module.Web/ButtonDetailViewItemWeb.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DevExpress.ExpressApp.Editors;
+using System.Web.UI.WebControls;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Layout;
+
+namespace WebSolution.Module.Web {
+    public interface IModelButtonDetailViewItemWeb : IModelViewItem { }
+    [DetailViewItemAttribute(typeof(IModelButtonDetailViewItemWeb))]
+    public class ButtonDetailViewItemWeb : ViewItem {
+        public ButtonDetailViewItemWeb(IModelViewItem model, Type objectType)
+			: base(objectType, model.Id) {
+			CreateControl();
+		}
+        protected override object CreateControlCore()
+        {
+            ASPxButton button = new ASPxButton();
+            button.Text = "MyButton Two";
+            button.EnableClientSideAPI = true;
+            button.Click += new EventHandler(button_Click);                        
+            return button;
+        }
+        void button_Click(object sender, EventArgs e) {
+            // Perform the required actions on the server side.
+        }
+    }
+}
