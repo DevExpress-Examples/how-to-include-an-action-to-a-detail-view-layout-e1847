d3101caa48aff294a94b9e5ace6cbdc375cdf96e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DetailViewButton.Win/WinApplication.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp;
+
+namespace DetailViewButton.Win {
+    public partial class DetailViewButtonWindowsFormsApplication : WinApplication {
+        public DetailViewButtonWindowsFormsApplication() {
+            InitializeComponent();
+        }
+
+        private void DetailViewButtonWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+    }
+}
---
+++ VB/DetailViewButton.Module.Web/ButtonDetailViewItemWeb.vb
@@ -0,0 +1,35 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.ExpressApp.Editors
+Imports System.Web.UI.WebControls
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Web.Editors.ASPx
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.ExpressApp.Layout
+
+Namespace WebSolution.Module.Web
+	Public Interface IModelButtonDetailViewItemWeb
+        Inherits IModelViewItem
+	End Interface
+	<DetailViewItemAttribute(GetType(IModelButtonDetailViewItemWeb))> _
+	Public Class ButtonDetailViewItemWeb
+		Inherits ViewItem
+        Public Sub New(ByVal model As IModelViewItem, ByVal objectType As Type)
+            MyBase.New(objectType, model.Id)
+            CreateControl()
+        End Sub
+		Protected Overrides Function CreateControlCore() As Object
+			Dim button As New ASPxButton()
+			button.Text = "MyButton Two"
+			button.EnableClientSideAPI = True
+			AddHandler button.Click, AddressOf button_Click
+			Return button
+		End Function
+		Private Sub button_Click(ByVal sender As Object, ByVal e As EventArgs)
+			' Perform the required actions on the server side.
+		End Sub
+	End Class
+End Namespace
