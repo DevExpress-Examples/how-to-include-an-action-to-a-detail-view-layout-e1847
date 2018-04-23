2483931bcf19d03a44f9b8d5dcd9d6250476ea6b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DetailViewButton.Module/Updater.cs
@@ -0,0 +1,23 @@
+using System;
+
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.BaseImpl;
+
+namespace DetailViewButton.Module {
+    public class Updater : ModuleUpdater {
+        public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+
+            Person testPerson = ObjectSpace.FindObject<Person>(CriteriaOperator.Parse("FirstName == 'Sam'"));
+            if(testPerson == null) {
+                testPerson = ObjectSpace.CreateObject<Person>();
+                testPerson.FirstName = "Sam";
+                testPerson.Save();
+            }
+        }
+    }
+}
