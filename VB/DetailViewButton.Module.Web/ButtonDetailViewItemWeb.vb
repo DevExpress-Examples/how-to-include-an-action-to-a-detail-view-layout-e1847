70c4f9c7d48206b8226c09512da3303a2938f225
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DetailViewButton.Module.Web/ButtonDetailViewItemWeb.vb
@@ -0,0 +1,34 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.ExpressApp.Editors
+Imports System.Web.UI.WebControls
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.ExpressApp.Web.Editors.ASPx
+
+Namespace WebSolution.Module.Web
+    Public Interface IModelButtonDetailViewItemWeb
+        Inherits IModelDetailViewItem
+    End Interface
+    <DetailViewItemAttribute(GetType(IModelButtonDetailViewItemWeb))> _
+    Public Class ButtonDetailViewItemWeb
+        Inherits ViewItem
+        Public Sub New(ByVal model As IModelDetailViewItem, ByVal objectType As Type)
+            MyBase.New(objectType, model.Id)
+            CreateControl()
+        End Sub
+        Protected Overrides Function CreateControlCore() As Object
+            Dim button As New ASPxButton()
+            button.Text = "MyButton Two"
+            button.EnableClientSideAPI = True
+            AddHandler button.Click, AddressOf button_Click
+            Return button
+        End Function
+        Private Sub button_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Perform the required actions on the server side.
+        End Sub
+    End Class
+End Namespace
